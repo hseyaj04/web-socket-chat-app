35af864bc06c5de14de1475009aa72957b0ed867
Hseyaj <codex.hseyaj26@gmail.com>
2025-03-26 22:22:21 +0530

    0. Intro 1. Project Initialization 2. Serving HTML 3. Integrating Socket.IO 4. Emitting events

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const { createServer} = require('node:http')
 const { Server } = require('socket.io')
+const {join} = require('node:path')
+
+
 
 const app = express()
-
 const server = createServer(app)
+const io = new Server(server)
 
 
 app.get('/', (req, res) => {
-    res.send("hello chat");
+    res.sendFile(join(__dirname, 'index.html'));
 })
 
-
+io.on('connection', (socket) => {
+    // console.log("a user connected");
+    socket.on('chat message', (msg) => {
+        console.log(msg);
+    })
+})
 
 server.listen(3000, () => {
     console.log("listening at port http://localhost:3000");
