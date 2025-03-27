cee658d45d713fb316368685defc2e2cc6909102
Hseyaj <codex.hseyaj26@gmail.com>
2025-03-27 12:22:30 +0530

    Server delivery

--- index.js
+++ index.js
@@ -2,28 +2,77 @@ const express = require('express')
 const { createServer} = require('node:http')
 const { Server } = require('socket.io')
 const {join} = require('node:path')
+const sqlite3 = require('sqlite3')
+const {open} = require('sqlite')
 
 
 
-const app = express()
-const server = createServer(app)
-const io = new Server(server, {connectionStateRecovery: {}})
 
+async function main(){
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+  })
+  
+  await db.exec(`
+      CREATE TABLE IF NOT EXISTS messages (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          client_offset TEXT UNIQUE,
+          content TEXT
+      );
+    `)
+  
+  const app = express()
+  const server = createServer(app)
+  const io = new Server(server, {connectionStateRecovery: {}})
+  
+  
+  app.get('/', (req, res) => {
+      res.sendFile(join(__dirname, 'index.html'));
+  })
+  
+  
+  io.emit('hello', 'world') //emits event to all connected sockets
+  
+  io.on('connection', async (socket) => {
+      socket.on('chat message', async (msg) => {
+        let result;
+        try {
+          result = await db.run('INSERT INTO messages (content) VALUES (?)', msg)
+        } catch (error) {
+          throw new Error(error);
+        }
+        io.emit('chat message', msg, result.lastID);
+      });
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-})
+      socket.on('connection message', async (msg) => {
+        let result;
+        try {
+          result = await db.run('INSERT INTO messages (content) VALUES (?)', msg)
+        } catch (error) {
+          throw new Error(error);
+        }
+        io.emit('connection message', msg, result.lastID);
+      });
 
+      if (!socket.recovered) {
+        // if the connection state recovery was not successful
+        try {
+          await db.each('SELECT id, content FROM messages WHERE id > ?',
+            [socket.handshake.auth.serverOffset || 0],
+            (_err, row) => {
+              socket.emit('chat message', row.content, row.id);
+            }
+          )
+        } catch (e) {
+          throw new Error(e)
+        }
+      }
+      });
+  
+  server.listen(3000, () => {
+      console.log("listening at port http://localhost:3000");
+  })
+}
 
-
-io.emit('hello', 'world') //emits event to all connected sockets
-
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-  });
-
-server.listen(3000, () => {
-    console.log("listening at port http://localhost:3000");
-})
+main();
